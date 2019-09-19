6c610b0eff7b42596d4681a81cd90e5346237114
rina2803 <buthnet92@gmail.com>
2019-09-19 11:58:29 +0300

    add commit pub

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,7 +1,20 @@
 class PublicationsController < ApplicationController
   def new
+    @publication = Publication.new
   end
   def create
+
+    @publication = Publication.create('content' => params[:content],  user_id: current_user.id)
+      respond_to do |format|
+      if @publication.save
+        format.html { redirect_to @publication, notice: 'PUblication was successfully created.' }
+        format.json { render :show, status: :created, location: @publication }
+      else
+        format.html { render :new }
+        format.json { render json: @publication.errors, status: :unprocessable_entity }
+      end
+        end
+
   end
   def edit
   end

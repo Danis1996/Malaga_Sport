60e6bc89fcabba7558c7a2223725312b648b8adf
Francette <mkt.curlyaho@gmail.com>
2019-09-21 11:50:14 +0300

    Modification event

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -2,7 +2,17 @@ class AvatarsController < ApplicationController
   def create
   	@user = User.find(params[:user_id])
     @user.avatar.attach(params[:avatar])
-    redirect_to(user_path(@user))
+    if params[:avatar] != nil
+      @user.avatar.attach(params[:avatar])
+      redirect_to(user_path(@user))
+    elsif @user.avatar.attached?
+      flash[:warning] = "Veuillez insérer une image"
+      redirect_to(user_path(@user.id))
+
+    else
+      flash[:warning] = "Veuillez insérer une image"
+      render 'new'
+    end
   end
 
   def new
